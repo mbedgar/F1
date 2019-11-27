d38a1f2a16f667fa99fa6d8f3b15e334a03c335b
Max Edgar <max.edgar@creditsafe.com>
2019-11-27 20:53:28 +0000

    Use internal docker dns name for influxdb

--- influxdb.go
+++ influxdb.go
@@ -11,7 +11,7 @@ const (
 	db       = "f1"
 	username = "admin"
 	password = "admin"
-	addr     = "192.168.5.7:8089"
+	addr     = "influxdb:8089"
 )
 
 // Point is a struct with data for sending to InfluxDB
